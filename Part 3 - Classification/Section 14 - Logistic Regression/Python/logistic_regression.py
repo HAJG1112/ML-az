9ef1f987da20deef0f2e146aee2b5cf20bd74ff2
Haisun Grigg <haisungrigg@Haisuns-MacBook-Air.local>
2023-09-24 02:31:52 +0100

    tweaks

--- Part 3 - Classification/Section 14 - Logistic Regression/Python/logistic_regression.py
+++ Part 3 - Classification/Section 14 - Logistic Regression/Python/logistic_regression.py
@@ -19,7 +19,7 @@ print(X_test)
 print(y_test)
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
